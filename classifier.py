703ddd5ca611253784f8c8762da17d275ec99a55
Adrian <adrian@ribe.no>
2018-05-03 14:28:01 +0200

    added support for confusion matrix on online verification

--- classifier.py
+++ classifier.py
@@ -3,6 +3,7 @@ from sklearn import svm
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import StandardScaler
@@ -18,7 +19,75 @@ import globalvar
 import itertools
 
 
+def onlineConfusion():
+    yTest = []
+    predictions = []
+    numberOfMovements = []
+    actualMovements = [
+                    [37,1,0,0,0], #Blink
+                    [2,60,7,2,1], #Centern
+                    [0,0,14,0,0], #Left
+                    [0,0,1,15,0], #Right
+                    [0,0,0,0,18], #Up
+                    ]
 
+    #Create actual prediction list
+
+    for i in range(len(actualMovements)):
+        counter = 0
+        for j in range(len(actualMovements[i])):
+            counter += actualMovements[i][j]
+            print("j %d" %j)
+            if actualMovements[i][j] == 0:
+                pass
+            else:
+                if j == 0:
+                    for z in range(actualMovements[i][j]):
+                        predictions.append(0)
+                elif j == 1:
+                    for z in range(actualMovements[i][j]):
+                        predictions.append(5)
+                elif j == 2:
+                    for z in range(actualMovements[i][j]):
+                        predictions.append(4)
+                elif j == 3:
+                    for z in range(actualMovements[i][j]):
+                        predictions.append(6)
+                else:
+                    for z in range(actualMovements[i][j]):
+                        predictions.append(8)
+        numberOfMovements.append(counter)
+    #Create the solution
+    for i in range(len(numberOfMovements)):
+        if i == 0:
+            for j in range(numberOfMovements[i]):
+                yTest.append(0)
+        elif i == 1:
+            for j in range(numberOfMovements[i]):
+                yTest.append(5)
+        elif i == 2:
+            for j in range(numberOfMovements[i]):
+                yTest.append(4)
+        elif i == 3:
+            for j in range(numberOfMovements[i]):
+                yTest.append(6)
+        else:
+            for j in range(numberOfMovements[i]):
+                yTest.append(8)
+
+
+    print("Accuracy Score: \n")
+    print(accuracy_score(yTest, predictions))
+    print("Precision Score: \n")
+    print(precision_score(yTest, predictions, average = 'macro'))
+    print("Recall Score: \n")
+    print(recall_score(yTest, predictions, average = 'macro'))
+    print("\n")
+    print(classification_report(yTest, predictions))
+    print("number of max true positives: ")
+    print(numberOfMovements)
+    confusionMatrix = confusion_matrix(yTest, predictions, labels = [0,5,4,6,8])
+    plotConfusionMatrix(confusionMatrix, ["blink","straight", "left", "right", "up"], normalize = True)
 
 def predict(Xtest, clf, yTest):
     print("Starting to predict")
@@ -27,7 +96,6 @@ def predict(Xtest, clf, yTest):
     stop = time.time()
     print("Time taken to predict with given examples:")
     print(stop-start)
-
     #Print the test data to see how well it performs.
     confusionMatrix = confusion_matrix(yTest, predictions, labels = [0,5,2,4,6,8])
     plotConfusionMatrix(confusionMatrix, ["blink","straight", "down", "left", "right", "up"])
@@ -92,6 +160,7 @@ def plotConfusionMatrix(cm, classes,
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm = cm*100
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
@@ -105,7 +174,7 @@ def plotConfusionMatrix(cm, classes,
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
 
-    fmt = '.2f' if normalize else 'd'
+    fmt = '.0f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, format(cm[i, j], fmt),
@@ -262,3 +331,6 @@ def loadMachineState(string):
 
     clf = joblib.load("Classifiers"+slash + string + ".pkl")
     return clf
+
+if __name__ == '__main__':
+	onlineConfusion()
